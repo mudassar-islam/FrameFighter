37299f94bfba034f327418f06904f0b064b3694e
Mudasssar Islam <demoncious@gmail.com>
2025-12-19 20:46:51 +0500

    Initial

---
+++ project/scenes/playground.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var fighterInput = $FighterInput
+		
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	fighterInput.set_side(FrameFighter.PLAYER_ONE)
+	fighterInput.should_charge(true)
+	
+	pass # Replace with function body.
+
+var accum = 0;
+
+func _physics_process(delta: float) -> void:
+	accum += delta
+	
+	if accum > 0.75:
+		accum = 0
+		fighterInput.tick()
+	pass
